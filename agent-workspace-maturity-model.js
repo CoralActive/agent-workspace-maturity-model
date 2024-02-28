249329bc9e77e58769066a12ce1df602d704e126
Andrew Hunt <161653708+andrew-hunt-coral@users.noreply.github.com>
2024-02-29 09:53:36 +1100

    Create initial documents

---
+++ agent-workspace-maturity-model.js
@@ -0,0 +1,97 @@
+const modelData = {
+  "Single-pane-of-glass": [
+    [
+      "No integration (agents \"alt-tab\" between windows)"
+    ],
+    [
+      "CRM connectors",
+      "Contact Center software/CCaaS connectors"
+    ],
+    [
+      "REST APIs"
+    ],
+    [
+      "Broad APls support: SOAP, Web Sockets, JavaScript, etc."
+    ]
+  ],
+  "Customization": [
+    [
+      "No customization (each workflow requires agents to navigate through each application window)"
+    ],
+    [
+      "Single window",
+      "Apps integrated in tabs",
+      "Scrolling might still be required",
+      "Workspace can be branded"
+    ],
+    [
+      "Choice of the container app",
+      "Applications integrated in tiles/widgets",
+    ],
+    [
+      "Toolkit to build your own widget and federate apps",
+      "Dynamic widgets (contextual)",
+      "Control over the focus area"
+    ]
+  ],
+  "Desktop Automation": [
+    [
+      "No automation (agents manually copy and paste information)"
+    ],
+    [
+      "High-volume/simple task automation using Robotic Desktop Automation/Attended RPA"
+    ],
+    [
+      "Low-code workflow automation"
+    ],
+    [
+      "Event-driven orchestration"
+    ]
+  ],
+  "Assistance & Guidance": [
+    [
+      "No assistance (agents rely on post-it notes and notepad cheat sheets)"
+    ],
+    [
+      "Agent scripting and decision trees"
+    ],
+    [
+      "Choice of Al models for providing guidance (contextual clues) and/or assistance (knowledge prompts)"
+    ],
+    [
+      "Orchestration of multiple Al and decision tree models",
+      "WCAG-compliant access"
+    ]
+  ],
+  "User Adoption": [
+    [
+      "No changes possible (agents use the various applications as-is)"
+    ],
+    [
+      "Small changes can be made",
+      "Waterfall approach with full OA cycle Agile with monthly iteration required at each iteration"
+    ],
+    [
+      "Agile with monthly iteration",
+      "Testing automated, at least partially"
+    ],
+    [
+      "Rapid test/validation framework"
+    ]
+  ],
+  "Cloud Migration": [
+    [
+      "The desktop needs to be altered or adapted at each stage of the migration"
+    ],
+    [
+      "CRM and Contact Center connectors can be swapped"
+    ],
+    [
+      "Supports multiple contact center (on premises) and CCaaS environments simultaneously",
+      "Supports multiple CRM environments"
+    ],
+    [
+      "Support for application swapping and insertion"
+    ]
+  ]
+}
---
+++ load.js
@@ -0,0 +1,63 @@
+
+$(function () {
+  const header = `
+      <thead>
+        <tr>
+          <th class="blank"></th>
+          <th class="patchwork">
+            Patchwork
+          </th>
+          <th class="integrated">
+           Integrated 
+           <div class="subheading"> (Patchwork +)</div>
+          </th>
+          <th class="unified">    
+            Unified 
+            <div class="subheading"> (Integrated +)</div> 
+          </th>
+          <th class="composable"> 
+            Composable 
+            <div class="subheading"> (Unified +)</div>
+          </th>
+        </tr>
+      </thead>
+    `;
+
+  function ul(arr) {
+    return "<ul>" + arr.map(text => `<li><span class='tick'>&#x2714;</span>  ${text}</li>`).join("") + "</ul>"
+  }
+
+
+  const body = $("<tbody>").append(
+    Object.keys(modelData).map(row => {
+      return $("<tr>")
+        .append($("<td>").append(row))
+        .append($("<td>").append(ul(modelData[row][0])))
+        .append($("<td>").append(ul(modelData[row][1])))
+        .append($("<td>").append(ul(modelData[row][2])))
+        .append($("<td>").append(ul(modelData[row][3])))
+    })
+  )
+
+  const caption = $(`
+      <caption>
+        <div style="float: left">Ed 1.0</div>
+
+        <vdiv style="float: center">
+        Customer-facing employee workspace maturity model 
+        is openly licensed via <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons BY 4.0</a>.
+        </div>
+
+        <vdiv style="float: right">
+        <a href="https://www.coralactive.com" target="_blank">&copy; 2024 Coral Active</a>
+        </span>
+      </caption>
+    `)
+
+  const table = $("<table>")
+    .append(caption)
+    .append(header)
+    .append(body)
+
+  $("#maturity-model").append(table)
+});
